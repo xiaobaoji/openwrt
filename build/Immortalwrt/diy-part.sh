9ec26d33185e9882ec6413fb367eb4427ff7992e
xiaobaoji <57552340+xiaobaoji@users.noreply.github.com>
2024-10-31 16:57:23 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -93,10 +93,10 @@ sed -i 's/"管理权"/"管理员"/g' `egrep "管理权" -rl ./`
 sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' `egrep "阿里云盘 WebDAV" -rl ./`
 sed -i 's/"UPnP"/"UPnP设置"/g' `egrep "UPnP" -rl ./`
 sed -i 's/"诊断"/"网络诊断"/g' `egrep "诊断" -rl ./`
-sed -i 's/"Argon设置"/"主题设置"/g' `egrep "Argon设置" -rl ./`
+#sed -i 's/"Argon设置"/"主题设置"/g' `egrep "Argon设置" -rl ./`
 #sed -i 's/"重启"/"重启项"/g' `egrep "重启" -rl ./`
 #sed -i 's/"关机"/"关闭电源"/g' `egrep "关机" -rl ./`
-sed -i 's/"备份与升级"/"备份/升级"/g' `egrep "备份与升级" -rl ./`
+#sed -i 's/"备份与升级"/"备份/升级"/g' `egrep "备份与升级" -rl ./`
 #sed -i 's/"DHCP/DNS"/"转发"/g' `egrep "DHCP/DNS" -rl ./`
 #sed -i 's/"网络唤醒Plus"/"网络唤醒"/g' `egrep "网络唤醒Plus" -rl ./`
 sed -i 's/"网络存储"/"NAS"/g' `egrep "网络存储" -rl ./`
