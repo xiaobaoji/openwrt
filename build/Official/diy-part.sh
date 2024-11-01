6bc85925ef6e20fd4f3fac6a98b00c295fa5ec30
xiaobaoji <57552340+xiaobaoji@users.noreply.github.com>
2024-11-01 13:08:34 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -87,9 +87,10 @@ sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `egrep "Turbo ACC 网络加
 sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
 #sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
 #sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
-#sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
+#sed -i 's/"MihomoTProxy"/"Mihomo"/g' `egrep "MihomoTProxy" -rl ./`
 
 # 修改插件名字
+sed -i 's/"MihomoTProxy"/"Mihomo"/g' `egrep "MihomoTProxy" -rl ./`
 sed -i 's/"KMS 服务器"/"KMS 激活"/g' `egrep "KMS 服务器" -rl ./`
 sed -i 's/"Web 管理"/"管理"/g' `egrep "Web 管理" -rl ./`
 sed -i 's/"管理权"/"管理员"/g' `egrep "管理权" -rl ./`
